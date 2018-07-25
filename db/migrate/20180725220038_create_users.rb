9cc89b65af280b1a7d7fd7c9966a23a540c56f8c
Jed Peek <jedpeek@yahoo.com>
2018-07-25 22:04:55 +0000

    automatically backed up by learn

--- db/migrate/20180725220038_create_users.rb
+++ db/migrate/20180725220038_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :Users do |t|
+    create_table :users do |t|
       t.string :name
-      t.password :password
       t.string :email
+      t.string :password
     end
   end
 end
