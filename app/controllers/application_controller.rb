24758d6c438850a0107bf84a142405b5ab2886fc
Jed Peek <jedpeek@yahoo.com>
2018-07-25 22:35:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
   get '/users/home' do
     @user = User.find(session[:id])
     if @user == nil
-      redirect to 'sessions/login'
+      redirect to '/sessions/login'
     erb :'/users/home'
   end
 
